eff02b5eb2d3004c8bcb3a0cce0d9a5b304d1d92
Isaac Grey <isaacgrey@Isaacs-MacBook-Pro-2.local>
2018-08-28 20:48:46 -0700

    added prod settings

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port)
